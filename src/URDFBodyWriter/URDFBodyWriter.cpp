d36403046787e8fa8e3163e71b0a7bee7b795d19
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2023-04-24 21:10:46 +0900

    [WIP] update

--- src/URDFBodyWriter/URDFBodyWriter.cpp
+++ src/URDFBodyWriter/URDFBodyWriter.cpp
@@ -1,5 +1,11 @@
 #include "URDFBodyWriter.h"
 
+#include <cnoid/NullOut>
+
+#include <pugixml.hpp>
+
+#include <set>
+
 using namespace cnoid;
 //using fmt::format;
 //namespace filesystem = cnoid::stdx::filesystem;
@@ -8,10 +14,64 @@ namespace cnoid {
 
 class URDFBodyWriter::Impl
 {
+public:
+    Impl();
+
+    bool writeBody(Body *body, const std::string& fname);
+
+    std::ostream* os_;
+    std::ostream& os() { return *os_; }
+
+    bool use_xacro;
+    bool use_geometry;
+    bool use_offset;
+
+    std::string robot_name;
+protected:
+    void addXacroScripts(pugi::xml_node &_node);
+    void addTransMission(pugi::xml_node &_node, const std::string &_joint_name, bool useXacro);
+
+    void addGeometry(pugi::xml_node &_node, const std::string &_filee_name, bool isVisual);
+    void addMassParam(pugi::xml_node &_node, const cnoid::Link *_lk);
+
+    //void addLink
+    //void addJoint
 };
 
 };  // namespace cnoid
+URDFBodyWriter::Impl::Impl()
+{
+    os_ = &nullout();
+    use_xacro = true;
+    use_geometry = true;
+    use_offset = true;
+}
+void URDFBodyWriter::Impl::addXacroScripts(pugi::xml_node &_node)
+{
+}
+void URDFBodyWriter::Impl::addGeometry(pugi::xml_node &_node, const std::string &_filee_name, bool isVisual)
+{
+}
+void URDFBodyWriter::Impl::addMassParam(pugi::xml_node &_node, const cnoid::Link *_lk)
+{
+}
+void URDFBodyWriter::Impl::addTransMission(pugi::xml_node &_node, const std::string &_joint_name, bool useXacro)
+{
+}
+bool URDFBodyWriter::Impl::writeBody(Body* body, const std::string& fname)
+{
+    pugi::xml_document doc;
+    pugi::xml_node root = doc.append_child("robot");
 
+    root.append_attribute("name") = "robot_name";
+
+    if (use_xacro) {
+        addXacroScripts(root);
+    }
+
+    doc.save_file(fname.c_str());
+    return true;
+}
 URDFBodyWriter::URDFBodyWriter()
 {
     impl = new Impl();
@@ -20,12 +80,11 @@ URDFBodyWriter::~URDFBodyWriter()
 {
     delete impl;
 }
-
 void URDFBodyWriter::setMessageSink(std::ostream& os)
 {
+    impl->os_ = &os;
 }
-
 bool URDFBodyWriter::writeBody(Body* body, const std::string& filename)
 {
-    return true;
+    return impl->writeBody(body, filename);
 }
--- src/URDFBodyWriter/test_xml.cpp
+++ src/URDFBodyWriter/test_xml.cpp
@@ -9,6 +9,54 @@ int main(int argc, char **argv)
     pugi::xml_node root = doc.append_child("robot");
 
     {
+        root.append_attribute("name") = "robot_name";
+        root.append_attribute("xmlns:xi") = "http://www.w3.org/2001/Xinclude";
+        root.append_attribute("xmlns:gazebo"    ) = "http://playerstage.sourceforge.net/gazebo/xmlschema/#gz";
+        root.append_attribute("xmlns:model"     ) = "http://playerstage.sourceforge.net/gazebo/xmlschema/#model";
+        root.append_attribute("xmlns:sensor"    ) = "http://playerstage.sourceforge.net/gazebo/xmlschema/#sensor";
+        root.append_attribute("xmlns:body"      ) = "http://playerstage.sourceforge.net/gazebo/xmlschema/#body";
+        root.append_attribute("xmlns:geom"      ) = "http://playerstage.sourceforge.net/gazebo/xmlschema/#geom";
+        root.append_attribute("xmlns:joint"     ) = "http://playerstage.sourceforge.net/gazebo/xmlschema/#joint";
+        root.append_attribute("xmlns:interface" ) = "http://playerstage.sourceforge.net/gazebo/xmlschema/#interface";
+        root.append_attribute("xmlns:rendering" ) = "http://playerstage.sourceforge.net/gazebo/xmlschema/#rendering";
+        root.append_attribute("xmlns:renderable") = "http://playerstage.sourceforge.net/gazebo/xmlschema/#renderable";
+        root.append_attribute("xmlns:controller") = "http://playerstage.sourceforge.net/gazebo/xmlschema/#controller";
+        root.append_attribute("xmlns:physics"   ) = "http://playerstage.sourceforge.net/gazebo/xmlschema/#physics";
+        root.append_attribute("xmlns:xacro"     ) = "http://www.ros.org/wiki/xacro";
+    }
+
+    {// add trans xacro
+        pugi::xml_node xac = root.append_child("xacro:macro");
+
+        xac.append_attribute("name") = "gazobo_transmission";
+        xac.append_attribute("params") = "joint_name";
+
+        pugi::xml_node trans = xac.append_child("transmission");
+        trans.append_attribute("name") = "${joint_name}_trans";
+
+        trans.append_child("type")
+            .append_child(pugi::node_pcdata)
+            .set_value("transmission_interface/SimpleTransmission");
+
+        pugi::xml_node t_joint = trans.append_child("joint");
+        t_joint.append_attribute("name") = "${joint_name}";
+        t_joint.append_child("hardwareInterface")
+            .append_child(pugi::node_pcdata)
+            .set_value("hardware_interface/PositionJointInterface</hardwareInterface");
+
+        pugi::xml_node t_act   = trans.append_child("actuator");
+        t_act.append_attribute("name") = "${joint_name}_motor";
+        t_act.append_child("hardwareInterface")
+            .append_child(pugi::node_pcdata)
+            .set_value("hardware_interface/PositionJointInterface</hardwareInterface");
+    }
+
+    {
+        pugi::xml_node comment = root.append_child(pugi::node_comment);
+        comment.set_value("hogemoge");
+    }
+
+    {
         pugi::xml_node nd = root.append_child("desc");
         nd.append_attribute("name") = "at_name";
         nd.append_attribute("val(int)") = 1;
