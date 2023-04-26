7a481d3fb31af937b923e2d08f1bf054de1e5657
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2023-04-26 19:58:06 +0900

    [WIP] up

--- src/URDFBodyWriter/URDFBodyWriter.cpp
+++ src/URDFBodyWriter/URDFBodyWriter.cpp
@@ -1,14 +1,36 @@
 #include "URDFBodyWriter.h"
 
 #include <cnoid/NullOut>
+#include <cnoid/MeshExtractor>
+#include <cnoid/EigenUtil>
 
 #include <pugixml.hpp>
 
-#include <set>
-
+#include <sstream>
+#include <iomanip>
+// #include <set>
 using namespace cnoid;
 //using fmt::format;
 //namespace filesystem = cnoid::stdx::filesystem;
+
+#define _SET_PRECISION(strm,prec_)                                      \
+    int prc;                                                            \
+    prc = strm.precision();                                             \
+    strm << std::scientific << std::setprecision(prec_);                \
+
+#define _UNSET_PRECISION(strm)                                          \
+    strm << std::fixed << std::setprecision(prc);                       \
+
+//// inline functions for printing
+inline void _print_vector(Vector3 &_vec, std::ostream &ostrm)
+{
+    _SET_PRECISION(ostrm,12);
+    ostrm << "\"";
+    ostrm << _vec.x() << " ";
+    ostrm << _vec.y() << " ";
+    ostrm << _vec.z() << "\"";
+    _UNSET_PRECISION(ostrm);
+}
 
 namespace cnoid {
 
@@ -31,7 +53,7 @@ protected:
     void addXacroScripts(pugi::xml_node &_node);
     void addTransMission(pugi::xml_node &_node, const std::string &_joint_name, bool useXacro);
 
-    void addGeometry(pugi::xml_node &_node, const std::string &_filee_name, bool isVisual);
+    void addGeometry(pugi::xml_node &_node, const cnoid::Link *_lk, bool isVisual);
     void addMassParam(pugi::xml_node &_node, const cnoid::Link *_lk);
 
     //void addLink
@@ -49,7 +71,7 @@ URDFBodyWriter::Impl::Impl()
 void URDFBodyWriter::Impl::addXacroScripts(pugi::xml_node &_node)
 {
 }
-void URDFBodyWriter::Impl::addGeometry(pugi::xml_node &_node, const std::string &_filee_name, bool isVisual)
+void URDFBodyWriter::Impl::addGeometry(pugi::xml_node &_node, const cnoid::Link *_lk, bool isVisual)
 {
 }
 void URDFBodyWriter::Impl::addMassParam(pugi::xml_node &_node, const cnoid::Link *_lk)
@@ -62,9 +84,9 @@ bool URDFBodyWriter::Impl::writeBody(Body* _body, const std::string& _fname)
 {
     pugi::xml_document doc;
     pugi::xml_node root = doc.append_child("robot");
-    
+
     std::string robot_name = _body->modelName();
-    root.append_attribute("name") = robot_name;
+    root.append_attribute("name") = robot_name.c_str();
 
     if (use_xacro) {
         addXacroScripts(root);
@@ -72,26 +94,26 @@ bool URDFBodyWriter::Impl::writeBody(Body* _body, const std::string& _fname)
 
     for(int i = 0; i < _body->numLinks(); i++) {
         cnoid::Link *cur_lk = _body->link(i);
-	pugi::xml_node link_node =  root.append_child("link");
-	link_node.append_attribute("name") = cur_lk->name();
-	if (use_geometry) {
-	   // extract-mesh
-	   pugi::xml_node vis_node = link_node.append_child("visual");
-	   // extract-geom
-	   pugi::xml_node col_node = link_node.append_child("collision");
-	}
+        pugi::xml_node link_node =  root.append_child("link");
+        link_node.append_attribute("name") = cur_lk->name().c_str();
+        if (use_geometry) {
+            pugi::xml_node vis_node = link_node.append_child("visual");
+            addGeometry(vis_node, cur_lk, true);
+            pugi::xml_node col_node = link_node.append_child("collision");
+            addGeometry(col_node, cur_lk, false);
+        }
 
-	addMassParam(link_node, cur_lk);
+        addMassParam(link_node, cur_lk);
 
-	if(lk->isRoot()) {
-	  continue;
-	}
+        if(cur_lk->isRoot()) {
+            continue;
+        }
 
-	std::string _jtype = "fixed";
-        switch(lk->jointType()) {
+        std::string _jtype = "fixed";
+        switch(cur_lk->jointType()) {
         case cnoid::Link::RevoluteJoint:
             _jtype = "revolute";
-            if(lk->q_upper() > 1e16) {
+            if(cur_lk->q_upper() > 1e16) {
                 _jtype = "continuous";
             }
             break;
@@ -103,51 +125,51 @@ bool URDFBodyWriter::Impl::writeBody(Body* _body, const std::string& _fname)
             break;
             // _jtype = "planer"
         }
-	
-	pugi::xml_node joint_node =  root.append_child("joint");
-	joint_node.append_attribute("name") = cur_lk->jointName();
-	joint_node.append_attribute("type") = _jtype;
 
-	cnoid::Link *plk_ = cur_lk->parent();
+        pugi::xml_node joint_node =  root.append_child("joint");
+        joint_node.append_attribute("name") = cur_lk->jointName().c_str();
+        joint_node.append_attribute("type") = _jtype.c_str();
+
+        cnoid::Link *plk_ = cur_lk->parent();
         if(!!plk_) {
-  	  //pugi::xml_node pt_node = joint_node.append_child("parent");
-	  //pt_node.append_attribute("link") = plk_->name();
-	  joint_node.append_child("parent").append_attribute("link") = plk_->name();
+            //pugi::xml_node pt_node = joint_node.append_child("parent");
+            //pt_node.append_attribute("link") = plk_->name();
+            joint_node.append_child("parent").append_attribute("link") = plk_->name().c_str();
         }
-	//pugi::xml_node clk_node = joint_node.append_child("child");
-	//pt_node.append_attribute("link") = plk_->name();
-	joint_node.append_child("child").append_attribute("link") = cur_lk->name();
+        //pugi::xml_node clk_node = joint_node.append_child("child");
+        //pt_node.append_attribute("link") = plk_->name();
+        joint_node.append_child("child").append_attribute("link") = cur_lk->name().c_str();
 
-	if(use_offset) {
-            Isometry3::TranslationPart ltrans(lk->translation());
-            Isometry3::TranslationPart ptrans(plk->translation());
-            Matrix3 lrot(lk->rotation());
-            Matrix3 prot(plk->rotation());
+        if(use_offset) {
+            Isometry3::TranslationPart ltrans(cur_lk->translation());
+            Isometry3::TranslationPart ptrans(plk_->translation());
+            Matrix3 lrot(cur_lk->rotation());
+            Matrix3 prot(plk_->rotation());
 
             Matrix3 prot_inv = prot.transpose();
             Matrix3 trot = prot_inv * lrot;
             Vector3 _xyz = prot_inv * (ltrans - ptrans);
             Vector3 _rpy = cnoid::rpyFromRot(trot);
 
-	    pugi::xml_node o_node = joint_node.append_child("origin");
-	    //ostrm << "<origin xyz=";
+            pugi::xml_node o_node = joint_node.append_child("origin");
+            //ostrm << "<origin xyz=";
             //_print_vector(_xyz, ostrm);
             //ostrm << " rpy=";
             //_print_vector(_rpy, ostrm);
             //ostrm << "/>" << std::endl;
-	}
+        }
 
-	if(!lk->isFixedJoint()) {
+        if(!cur_lk->isFixedJoint()) {
             if(use_offset) {
                 Vector3 ax_ = cur_lk->a();
-		pugi::xml_node ax_node = joint_node.append_child("axis");
+                pugi::xml_node ax_node = joint_node.append_child("axis");
                 //_indent(4, ostrm);
                 //ostrm << "<axis xyz=";
                 //_print_vector(ax_, ostrm);
                 //ostrm << "/>" << std::endl;
             }
 
-	    pugi::xml_node lm_node = joint_node.append_child("limit");
+            pugi::xml_node lm_node = joint_node.append_child("limit");
             // _indent(4, ostrm);
             // ostrm << "<limit lower=";
             // _print_float(lk->q_lower(), ostrm);
@@ -160,7 +182,6 @@ bool URDFBodyWriter::Impl::writeBody(Body* _body, const std::string& _fname)
             // ostrm << "/>" << std::endl;
         }
     }
-    
     doc.save_file(_fname.c_str());
     return true;
 }
