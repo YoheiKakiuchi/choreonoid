64bace8b5e4e8f1fd9556dde9e5c573fa5904291
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2023-10-19 22:53:26 +0900

    [Util, PySceneDrawables.cpp] refactor Mesh,Plot methods

--- src/Util/pybind11/PySceneDrawables.cpp
+++ src/Util/pybind11/PySceneDrawables.cpp
@@ -33,50 +33,130 @@ void exportPySceneDrawables(py::module& m)
         .def_property("transparency", &SgMaterial::transparency, &SgMaterial::setTransparency)
         .def("setTransparency", &SgMaterial::setTransparency)
         ;
-
+// Comment for implementation of method
+// <vectorArray> xx, xxs
+// hasXXs     bool()
+// setXXs     void(Matrix)
+// xxs        property_ro Matrix()
+// sizeOfxxs  property_ro int()
+// appendXX   void(Vec)
+// xx         Vec(int)
+// setXX      void(int, Vec)
+// 
+// <Indices> XX
+// hasXXIndices bool()
+// setXXIndices void(vector<int>)
+// xxIndices property_ro vector<int>()
+// sizeOfXXIndices prop_ro int()
+// resizeXXIndices void(int)
+// getXXIndex int(int)
+// setXXIndex void(int, int)
     py::class_<SgMeshBase, SgMeshBasePtr, SgObject>(m, "SgMeshBase")
         .def("boundingBox", [](SgMeshBase &self) { BoundingBox ret = self.boundingBox(); return ret; })
         .def("updateBoundingBox", &SgMeshBase::updateBoundingBox)
+        // <VectorArray> vertex, vertices
         .def("hasVertices", &SgMeshBase::hasVertices)
-        .def("vertices", [](SgMeshBase &self) {
-             SgVertexArray *va_ = self.vertices();
-             Eigen::Matrix<float, -1, -1, Eigen::RowMajor> rm(va_->size(), 3);
-             int cntr = 0;
-             for(auto it = va_->begin(); it != va_->end(); it++, cntr++) rm.row(cntr) = *it;
-             return rm; })
+        .def("setVertices", [](SgMeshBase &self, RefMatrixfRM mat) {
+            // mat.cols() == 3
+            int size = mat.rows();
+            SgVertexArray *va_ = self.getOrCreateVertices(size);
+            for(int i = 0; i < size; i++) va_->at(i) = mat.row(i);  })
+        .def_property_readonly("vertices", [](SgMeshBase &self) {
+            SgVertexArray *va_ = self.vertices();
+            if (!va_) { MatrixfRM mat(0, 3); return mat; }
+            MatrixfRM mat(va_->size(), 3);
+            for(int i = 0; i < va_->size(); i ++) mat.row(i) = va_->at(i);
+            return mat; })
+        .def_property_readonly("sizeOfVertices", [](SgMeshBase &self) { if (!self.vertices()) return (std::size_t)0; return self.vertices()->size(); })
+        .def("appendVertex", [](SgMeshBase &self, const Vector3f &v) { self.vertices()->push_back(v); })
+        .def("vertex", [](SgMeshBase &self, int idx) { return self.vertices()->at(idx); })
+        .def("setVertex", [](SgMeshBase &self, int idx, const Vector3f &v) { self.vertices()->at(idx) = v; })
+        // <VectorArray> normal, normals
         .def("hasNormals", &SgMeshBase::hasNormals)
-        .def("normals", [](SgMeshBase &self) {
-             SgNormalArray *nm_ = self.normals();
-             Eigen::Matrix<float, -1, -1, Eigen::RowMajor> rm(nm_->size(), 3);
-             int cntr = 0;
-             for(auto it = nm_->begin(); it != nm_->end(); it++, cntr++) rm.row(cntr) = *it;
-             return rm; })
+        .def("setNormals", [](SgMeshBase &self, RefMatrixfRM mat) {
+            // mat.cols() == 3
+            int size = mat.rows();
+            SgNormalArray *va_ = self.getOrCreateNormals();
+            va_->resize(size);
+            for(int i = 0; i < size; i++) va_->at(i) = mat.row(i);  })
+        .def_property_readonly("normals",  [](SgMeshBase &self) {
+            SgNormalArray *va_ = self.normals();
+            if (!va_) { MatrixfRM mat(0, 3); return mat; }
+            MatrixfRM mat(va_->size(), 3);
+            for(int i = 0; i < va_->size(); i ++) mat.row(i) = va_->at(i);
+            return mat; })
+        .def_property_readonly("sizeOfNormals", [](SgMeshBase &self) { if (!self.normals()) return (std::size_t)0; return self.normals()->size(); })
+        .def("appendNormal", [](SgMeshBase &self, const Vector3f &v) { self.normals()->push_back(v); })
+        .def("normal", [](SgMeshBase &self, int idx) { return self.normals()->at(idx); })
+        .def("setNormal", [](SgMeshBase &self, int idx, const Vector3f &v) { self.normals()->at(idx) = v; })
+        // <VectorArray> color, colors
         .def("hasColors", &SgMeshBase::hasColors)
-        .def("colors", [](SgMeshBase &self) {
-             SgColorArray *col_ = self.colors();
-             Eigen::Matrix<float, -1, -1, Eigen::RowMajor> rm(col_->size(), 3);
-             int cntr = 0;
-             for(auto it = col_->begin(); it != col_->end(); it++, cntr++) rm.row(cntr) = *it;
-             return rm; })
+        .def("setColors", [](SgMeshBase &self, RefMatrixfRM mat) {
+            // mat.cols() == 3
+            int size = mat.rows();
+            SgColorArray *va_ = self.getOrCreateColors(size);
+            for(int i = 0; i < size; i++) va_->at(i) = mat.row(i);  })
+        .def_property_readonly("colors",  [](SgMeshBase &self) {
+            SgColorArray *va_ = self.colors();
+            if (!va_) { MatrixfRM mat(0, 3); return mat; }
+            MatrixfRM mat(va_->size(), 3);
+            for(int i = 0; i < va_->size(); i ++) mat.row(i) = va_->at(i);
+            return mat; })
+        .def_property_readonly("sizeOfColors", [](SgMeshBase &self) { if(!self.colors()) return (std::size_t)0;  return self.colors()->size(); })
+        .def("appendColor", [](SgMeshBase &self, const Vector3f &v) { self.colors()->push_back(v); })
+        .def("color", [](SgMeshBase &self, int idx) { return self.colors()->at(idx); })
+        .def("setColor", [](SgMeshBase &self, int idx, const Vector3f &v) { self.colors()->at(idx) = v; })
+        // <VectorArray> texcoord, texcoords
         .def("hasTexCoords", &SgMeshBase::hasTexCoords)
-        .def("texCoords", [](SgMeshBase &self) {
-             SgTexCoordArray *tc_ = self.texCoords();
-             Eigen::Matrix<float, -1, -1, Eigen::RowMajor> rm(tc_->size(), 2);
-             int cntr = 0;
-             for(auto it = tc_->begin(); it != tc_->end(); it++, cntr++) rm.row(cntr) = *it;
-             return rm; })
+        .def("setTexCoords", [](SgMeshBase &self, RefMatrixfRM mat) {
+            // mat.cols() == 3
+            int size = mat.rows();
+            SgTexCoordArray *va_ = self.getOrCreateTexCoords();
+            va_->resize(size);
+            for(int i = 0; i < size; i++) va_->at(i) = mat.row(i);  })
+        .def_property_readonly("texCoords",  [](SgMeshBase &self) {
+            SgTexCoordArray *va_ = self.texCoords();
+            if (!va_) { MatrixfRM mat(0, 2); return mat; }
+            MatrixfRM mat(va_->size(), 2);
+            for(int i = 0; i < va_->size(); i ++) mat.row(i) = va_->at(i);
+            return mat; })
+        .def_property_readonly("sizeOfTexCoords", [](SgMeshBase &self) { if(!self.texCoords()) return (std::size_t)0;  return self.texCoords()->size(); })
+        .def("appendTexCoord", [](SgMeshBase &self, const Vector2f &v) { self.texCoords()->push_back(v); })
+        .def("texCoord", [](SgMeshBase &self, int idx) { return self.texCoords()->at(idx); })
+        .def("setTexCoord", [](SgMeshBase &self, int idx, const Vector2f &v) { self.texCoords()->at(idx) = v; })
+        // <Indices> faceVertex
         .def("hasFaceVertexIndices", &SgMeshBase::hasFaceVertexIndices)
-        .def("faceVertexIndices", (SgIndexArray&(SgMeshBase::*)())&SgMeshBase::faceVertexIndices)
-        //.def("faceVertexIndices", [](SgMeshBase &self) { SgIndexArray idx_ = self.faceVertexIndices(); return idx_; })
+        .def("setFaceVertexIndices", [](SgMeshBase &self, std::vector<int> &_ind) { self.faceVertexIndices() = _ind; })
+        .def_property_readonly("faceVertexIndices", (SgIndexArray&(SgMeshBase::*)())&SgMeshBase::faceVertexIndices)
+        .def_property_readonly("sizeOfFaceVertexIndices", [](SgMeshBase &self){ return self.faceVertexIndices().size(); })
+        .def("resizeFaceVertexindices", [](SgMeshBase &self, int n) { self.faceVertexIndices().resize(n); })
+        .def("getFaceVertexindex", [](SgMeshBase &self, int idx) { return self.faceVertexIndices().at(idx); })
+        .def("setFaceVertexindex", [](SgMeshBase &self, int idx, int fceidx) { self.faceVertexIndices().at(idx) = fceidx; })
+        // <Indices> normal
         .def("hasNormalIndices", &SgMeshBase::hasNormalIndices)
-        .def("normalIndices", (SgIndexArray&(SgMeshBase::*)())&SgMeshBase::normalIndices)
-        //.def("normalIndices", [](SgMeshBase &self) { SgIndexArray idx_ = self.normalIndices(); return idx_; })
+        .def("setNormalIndices", [](SgMeshBase &self, std::vector<int> &_ind) { self.normalIndices() = _ind; })
+        .def_property_readonly("normalIndices", (SgIndexArray&(SgMeshBase::*)())&SgMeshBase::normalIndices)
+        .def_property_readonly("sizeOfNormalIndices", [](SgMeshBase &self){ return self.normalIndices().size(); })
+        .def("resizeNormalindices", [](SgMeshBase &self, int n) { self.normalIndices().resize(n); })
+        .def("getNormalindex", [](SgMeshBase &self, int idx) { return self.normalIndices().at(idx); })
+        .def("setNormalindex", [](SgMeshBase &self, int idx, int nmidx) { self.normalIndices().at(idx) = nmidx; })
+        // <Indices> color
         .def("hasColorIndices", &SgMeshBase::hasColorIndices)
-        .def("colorIndices", (SgIndexArray&(SgMeshBase::*)())&SgMeshBase::colorIndices)
-        //.def("colorIndices", [](SgMeshBase &self) { SgIndexArray idx_ = self.colorIndices(); return idx_; })
+        .def("setColorIndices", [](SgMeshBase &self, std::vector<int> &_ind) { self.colorIndices() = _ind; })
+        .def_property_readonly("colorIndices", (SgIndexArray&(SgMeshBase::*)())&SgMeshBase::colorIndices)
+        .def_property_readonly("sizeOfColorIndices", [](SgMeshBase &self){ return self.colorIndices().size(); })
+        .def("resizeColorindices", [](SgMeshBase &self, int n) { self.colorIndices().resize(n); })
+        .def("getColorindex", [](SgMeshBase &self, int idx) { return self.colorIndices().at(idx); })
+        .def("setColorindex", [](SgMeshBase &self, int idx, int colidx) { self.colorIndices().at(idx) = colidx; })
+        // <Indices> texcoord
         .def("hasTexCoordIndices", &SgMeshBase::hasTexCoordIndices)
-        .def("texCoordIndices", (SgIndexArray&(SgMeshBase::*)())&SgMeshBase::texCoordIndices)
-        //.def("texCoordIndices", [](SgMeshBase &self) { SgIndexArray idx_ = self.texCoordIndices(); return idx_; })
+        .def("setTexCoordIndices", [](SgMeshBase &self, std::vector<int> &_ind) { self.texCoordIndices() = _ind; })
+        .def_property_readonly("texCoordIndices", (SgIndexArray&(SgMeshBase::*)())&SgMeshBase::texCoordIndices)
+        .def_property_readonly("sizeOfTexCoordIndices", [](SgMeshBase &self){ return self.texCoordIndices().size(); })
+        .def("resizeTexCoordindices", [](SgMeshBase &self, int n) { self.texCoordIndices().resize(n); })
+        .def("getTexCoordindex", [](SgMeshBase &self, int idx) { return self.texCoordIndices().at(idx); })
+        .def("setTexCoordindex", [](SgMeshBase &self, int idx, int txcidx) { self.texCoordIndices().at(idx) = txcidx; })
+        //
         .def("creaseAngle", &SgMeshBase::creaseAngle)
         .def("isSolid", &SgMeshBase::isSolid)
         ;
@@ -103,11 +183,16 @@ void exportPySceneDrawables(py::module& m)
     sgMesh
         .def(py::init<>())
         .def("updateBoundingBox", &SgMesh::updateBoundingBox)
+        // <Indices> triangle == faceVertexIndices
         .def("triangleVertices", (SgIndexArray&(SgMesh::*)())&SgMesh::triangleVertices)
         //.def("triangleVertices", [](SgMesh &self) { SgIndexArray idx_ = self.triangleVertices(); return idx_; })
         .def("hasTriangles", &SgMesh::hasTriangles)
-        .def("numTriangles", &SgMesh::numTriangles)
+        .def("numTriangles", &SgMesh::numTriangles)// TODO property
         .def("triangle", [](SgMesh &self, int index) { Array3i ret = self.triangle(index); return ret; })
+        .def("setTriangle", &SgMesh::setTriangle)
+        .def("addTriangle", (void(SgMesh::*)(int, int, int))&SgMesh::addTriangle)
+        .def("setNumTriangles", &SgMesh::setNumTriangles)
+        .def("clearTriangles", &SgMesh::clearTriangles)
         .def_property("divisionNumber", &SgMesh::divisionNumber, &SgMesh::setDivisionNumber)
         .def_property("extraDivisionNumber", &SgMesh::extraDivisionNumber, &SgMesh::setExtraDivisionNumber)
         .def_property("extraDivisionMode", &SgMesh::extraDivisionMode, &SgMesh::setExtraDivisionMode)
@@ -203,7 +288,7 @@ void exportPySceneDrawables(py::module& m)
         .def_property("material", (SgMaterial* (SgPlot::*)()) &SgPlot::material, &SgPlot::setMaterial)
         .def("setMaterial", &SgPlot::setMaterial)
         .def("getOrCreateMaterial", &SgPlot::getOrCreateMaterial)
-        //vertex
+        // <VectorArray> vertex, vertices
         .def("hasVertices", &SgPlot::hasVertices)
         .def("setVertices", [](SgPlot &self, RefMatrixfRM mat) {
             // mat.cols() == 3
@@ -216,11 +301,11 @@ void exportPySceneDrawables(py::module& m)
             MatrixfRM mat(va_->size(), 3);
             for(int i = 0; i < va_->size(); i ++) mat.row(i) = va_->at(i);
             return mat; })
-        .def_property_readonly("sizeOfVertices", [](SgPlot &self) { return self.vertices()->size(); })
+        .def_property_readonly("sizeOfVertices", [](SgPlot &self) { if (!self.vertices()) return (std::size_t)0; return self.vertices()->size(); })
         .def("appendVertex", [](SgPlot &self, const Vector3f &v) { self.vertices()->push_back(v); })
         .def("vertex", [](SgPlot &self, int idx) { return self.vertices()->at(idx); })
         .def("setVertex", [](SgPlot &self, int idx, const Vector3f &v) { self.vertices()->at(idx) = v; })
-        //color
+        // <VectorArray> color, colors
         .def("hasColors", &SgPlot::hasColors)
         .def("setColors", [](SgPlot &self, RefMatrixfRM mat) {
             // mat.cols() == 3
@@ -233,19 +318,19 @@ void exportPySceneDrawables(py::module& m)
             MatrixfRM mat(va_->size(), 3);
             for(int i = 0; i < va_->size(); i ++) mat.row(i) = va_->at(i);
             return mat; })
-        .def_property_readonly("sizeOfColors", [](SgPlot &self) { return self.colors()->size(); })
+        .def_property_readonly("sizeOfColors", [](SgPlot &self) { if(!self.colors()) return (std::size_t)0;  return self.colors()->size(); })
         .def("appendColor", [](SgPlot &self, const Vector3f &v) { self.colors()->push_back(v); })
         .def("color", [](SgPlot &self, int idx) { return self.colors()->at(idx); })
         .def("setColor", [](SgPlot &self, int idx, const Vector3f &v) { self.colors()->at(idx) = v; })
-        //colorIndex
+        // <Indices> color
         .def("hasColorIndices", [](SgPlot &self) { return !self.colorIndices().empty(); })
-        .def_property("colorIndices", (SgIndexArray&(SgPlot::*)())&SgPlot::colorIndices,
-                      [](SgPlot &self, std::vector<int> &_ind) { self.colorIndices() = _ind; })
+        .def("setColorIndices", [](SgPlot &self, std::vector<int> &_ind) { self.colorIndices() = _ind; })
+        .def_property_readonly("colorIndices", (SgIndexArray&(SgPlot::*)())&SgPlot::colorIndices)
         .def_property_readonly("sizeOfColorIndices", [](SgPlot &self){ return self.colorIndices().size(); })
         .def("resizeColorIndices", [](SgPlot &self, int n) { self.colorIndices().resize(n); })
-        .def("getColorIndex", [](SgPlot &self, int idx) { self.colorIndices().at(idx); })
+        .def("getColorIndex", [](SgPlot &self, int idx) { return self.colorIndices().at(idx); })
         .def("setColorIndex", [](SgPlot &self, int idx, int colidx) { self.colorIndices().at(idx) = colidx; })
-        //normal
+         // <VectorArray> normal, normals
         .def("hasNormals", &SgPlot::hasNormals)
         .def("setNormals", [](SgPlot &self, RefMatrixfRM mat) {
             // mat.cols() == 3
@@ -259,17 +344,17 @@ void exportPySceneDrawables(py::module& m)
             MatrixfRM mat(va_->size(), 3);
             for(int i = 0; i < va_->size(); i ++) mat.row(i) = va_->at(i);
             return mat; })
-        .def_property_readonly("sizeOfNormals", [](SgPlot &self) { return self.normals()->size(); })
+        .def_property_readonly("sizeOfNormals", [](SgPlot &self) { if (!self.normals()) return (std::size_t)0; return self.normals()->size(); })
         .def("appendNormal", [](SgPlot &self, const Vector3f &v) { self.normals()->push_back(v); })
         .def("normal", [](SgPlot &self, int idx) { return self.normals()->at(idx); })
         .def("setNormal", [](SgPlot &self, int idx, const Vector3f &v) { self.normals()->at(idx) = v; })
-        //normalIndex
+         // <Indices> normal
         .def("hasNormalIndices", [](SgPlot &self) { return !self.normalIndices().empty(); })
-        .def_property("normalIndices", (SgIndexArray&(SgPlot::*)())&SgPlot::normalIndices,
-                      [](SgPlot &self, std::vector<int> &_ind) { self.normalIndices() = _ind; })
+        .def("setNormalIndices", [](SgPlot &self, std::vector<int> &_ind) { self.normalIndices() = _ind; })
+        .def_property_readonly("normalIndices", (SgIndexArray&(SgPlot::*)())&SgPlot::normalIndices)
         .def_property_readonly("sizeOfNormalIndices", [](SgPlot &self){ return self.normalIndices().size(); })
         .def("resizeNormalIndices", [](SgPlot &self, int n) { self.normalIndices().resize(n); })
-        .def("getNormalIndex", [](SgPlot &self, int idx) { self.normalIndices().at(idx); })
+        .def("getNormalIndex", [](SgPlot &self, int idx) { return self.normalIndices().at(idx); })
         .def("setNormalIndex", [](SgPlot &self, int idx, int nomidx) { self.normalIndices().at(idx) = nomidx; })
         ;
 
@@ -280,6 +365,8 @@ void exportPySceneDrawables(py::module& m)
 
     py::class_<SgLineSet, SgLineSetPtr, SgPlot>(m, "SgLineSet")
         .def(py::init<>())
+        // <Indices> lineVertex
+        .def("setLineVertexIndices", [](SgLineSet &self, std::vector<int> &_ind) { self.lineVertexIndices() = _ind; })
         .def_property("lineVertexIndices", (SgIndexArray&(SgLineSet::*)())&SgLineSet::lineVertexIndices,
                       [](SgLineSet &self, std::vector<int> &_ind) { self.lineVertexIndices() = _ind; })
         .def_property("numLines", &SgLineSet::numLines, &SgLineSet::setNumLines)
