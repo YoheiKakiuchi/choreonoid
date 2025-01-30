1cf0e4fa5227853600dbdc52aa7248a081061527
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2025-01-30 19:06:54 +0900

    [body2urdf] change option parser to CLI11

--- src/URDFBodyWriter/body2urdf.cpp
+++ src/URDFBodyWriter/body2urdf.cpp
@@ -2,16 +2,33 @@
 #include <cnoid/BodyLoader>
 #include <cnoid/Body>
 
+#ifdef USE_OLD_OPTION
 #include <boost/program_options.hpp>
+#else
+#include <CLI11.hpp>
+#endif
 
 #include <iostream>
 
 using namespace cnoid;
+#ifdef USE_OLD_OPTION
 using namespace boost::program_options;
+#endif
 
 int main(int argc, char **argv)
 {
-
+    std::string fname;
+    std::string outputf;
+    std::string rname;
+    bool no_offset = false;
+    bool no_geom   = false;
+    bool no_xacro  = false;
+    bool verbose   = false;
+    bool export_devices = false;
+    bool use_primitive = true;
+    std::string file_prefix_;
+    std::string url_prefix_;
+#ifdef USE_OLD_OPTION
     options_description desc("$ body2urdf [options] inputfile.body");
     desc.add_options()
     ("robot-name,r", value<std::string>(), "robot name")
@@ -37,24 +54,10 @@ int main(int argc, char **argv)
         return 0;
     }
 
-    std::string fname;
     for (auto const& str : collect_unrecognized(presult.options, include_positional)) {
         fname = str;
     }
 
-    if(fname.empty()) {
-        std::cerr << "Error! / please input filename" << std::endl;
-        std::cerr << "$ body2urdf [options] inputfile.body" << std::endl;
-        return -1;
-    }
-    std::string outputf;
-    std::string rname;
-    bool no_offset = false;
-    bool no_geom   = false;
-    bool no_xacro  = false;
-    bool verbose   = false;
-    bool export_devices = false;
-    bool use_primitive = true;
     if (vm.count("output") > 0) {
         outputf = vm["option"].as<std::string>();
     }
@@ -79,8 +82,35 @@ int main(int argc, char **argv)
     if (vm.count("export-devices") > 0) {
         export_devices = true;
     }
+
+    file_prefix_ = vm["mesh-file-prefix"].as<std::string>();
+    url_prefix_ = vm["mesh-url-prefix"].as<std::string>();
+#else
+    CLI::App vm{"Converting .body to .urdf"};
+    bool no_primitive{false};
+    vm.add_option("-r,--robot-name", rname, "robot name");
+    vm.add_option("-O,--output", outputf, "output file");
+    vm.add_option("--mesh-file-prefix", file_prefix_, "prefix of mesh file (e.g. directory)");
+    vm.add_option("--mesh-url-prefix", url_prefix_, "prefix of url refered in output URDF file");
+    vm.add_flag("--no-offset", no_offset, "surpress output about link position ");
+    vm.add_flag("--no-geometry", no_geom, "suppress output about geometry-info");
+    vm.add_flag("--no-xacro", no_xacro, "do not use xacro");
+    vm.add_flag("--no-primitive", no_primitive, "do not use primitive type of geometry");
+    vm.add_flag("--export-devices", export_devices, "export devices as link");
+    vm.add_flag("-v,--verbose", verbose, "verbose message");
+
+    vm.add_option("in_file_name", fname, "name of input file(.body)");
+    CLI11_PARSE(vm, argc, argv);
+
+    use_primitive = !no_primitive;
+#endif
+    /////
+    if(fname.empty()) {
+        std::cerr << "Error! / please input filename" << std::endl;
+        return -1;
+    }
     if (verbose) {
-        std::cerr << "fname: " << fname << std::endl;
+        std::cerr << "in_file_name: " << fname << std::endl;
         std::cerr << "robot name:     " << rname << std::endl;
         std::cerr << "no-offset:      "   << no_offset << std::endl;
         std::cerr << "no-geometry:    " << no_geom << std::endl;
@@ -115,10 +145,8 @@ int main(int argc, char **argv)
     ubw.setExportDevices(export_devices);
     ubw.setUseURDFPrimitiveGeometry(use_primitive);
 
-    std::string fp_ = vm["mesh-file-prefix"].as<std::string>();
-    ubw.setMeshFilePrefix(fp_);
-    std::string up_ = vm["mesh-url-prefix"].as<std::string>();
-    ubw.setMeshURLPrefix(up_);
+    ubw.setMeshFilePrefix(file_prefix_);
+    ubw.setMeshURLPrefix(url_prefix_);
     if(!rname.empty()){
         ubw.setRobotName(rname);
     }
