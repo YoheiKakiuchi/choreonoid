38d95bd382a4d637e5f350178be5b38ae62fc0e4
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2025-03-10 13:29:07 +0900

    [Body, pybind11] add devices, Force, Acceleration, RateGyro, Imu

--- src/Body/pybind11/PyDeviceTypes.cpp
+++ src/Body/pybind11/PyDeviceTypes.cpp
@@ -6,6 +6,9 @@
 #include "../Device.h"
 #include "../Link.h"
 #include "../ForceSensor.h"
+#include "../AccelerationSensor.h"
+#include "../RateGyroSensor.h"
+#include "../Imu.h"
 #include <cnoid/PyUtil>
 
 using namespace std;
@@ -49,6 +52,22 @@ void exportPyDeviceTypes(py::module& m)
         .def("getName", &Device::name)
         .def("getLink", (Link*(Device::*)())&Device::link)
         ;
+
+    py::class_<ForceSensor, ForceSensorPtr, Device>(m, "ForceSensor")
+    .def_property_readonly("f", [](const ForceSensor &self) { return self.f(); })
+    .def_property_readonly("tau", [](const ForceSensor &self) { return self.tau(); })
+    .def_property_readonly("wrench", [](const ForceSensor &self) { return self.wrench(); })
+    ;
+    py::class_<AccelerationSensor, AccelerationSensorPtr, Device>(m, "AccelerationSensor")
+    .def_property_readonly("dv", [](const AccelerationSensor &self) { return self.dv(); })
+    ;
+    py::class_<RateGyroSensor, RateGyroSensorPtr, Device>(m, "RateGyroSensor")
+    .def_property_readonly("w", [](const RateGyroSensor &self) { return self.w(); })
+    ;
+    py::class_<Imu, ImuPtr, Device>(m, "Imu")
+    .def_property_readonly("w", [](const Imu &self) { return self.w(); })
+    .def_property_readonly("dv", [](const Imu &self) { return self.dv(); })
+    ;
 
     PyDeviceList<Device>(m, "DeviceList");
     PyDeviceList<ForceSensor>(m, "ForceSensorList");
