d5850c4cf4d9911cc535c4750a8990575d6bd301
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2025-03-19 10:15:45 +0900

    [Body/pybind11] add device types and constructor

--- src/Body/pybind11/PyDeviceTypes.cpp
+++ src/Body/pybind11/PyDeviceTypes.cpp
@@ -9,6 +9,12 @@
 #include "../AccelerationSensor.h"
 #include "../RateGyroSensor.h"
 #include "../Imu.h"
+#include "../PointLight.h"
+#include "../SpotLight.h"
+#include "../MarkerDevice.h"
+#include "../ZmpDevice.h"
+#include "../Camera.h"
+#include "../RangeSensor.h"
 #include <cnoid/PyUtil>
 
 using namespace std;
@@ -54,19 +60,46 @@ void exportPyDeviceTypes(py::module& m)
         ;
 
     py::class_<ForceSensor, ForceSensorPtr, Device>(m, "ForceSensor")
+    .def(py::init<>())
     .def_property_readonly("f", [](const ForceSensor &self) { return self.f(); })
     .def_property_readonly("tau", [](const ForceSensor &self) { return self.tau(); })
     .def_property_readonly("wrench", [](const ForceSensor &self) { return self.wrench(); })
     ;
     py::class_<AccelerationSensor, AccelerationSensorPtr, Device>(m, "AccelerationSensor")
+    .def(py::init<>())
     .def_property_readonly("dv", [](const AccelerationSensor &self) { return self.dv(); })
     ;
     py::class_<RateGyroSensor, RateGyroSensorPtr, Device>(m, "RateGyroSensor")
+    .def(py::init<>())
     .def_property_readonly("w", [](const RateGyroSensor &self) { return self.w(); })
     ;
     py::class_<Imu, ImuPtr, Device>(m, "Imu")
+    .def(py::init<>())
     .def_property_readonly("w", [](const Imu &self) { return self.w(); })
     .def_property_readonly("dv", [](const Imu &self) { return self.dv(); })
+    ;
+
+    py::class_<Light, LightPtr, Device>(m, "Light")
+    ;
+    py::class_<PointLight, PointLightPtr, Light>(m, "PointLight")
+    .def(py::init<>())
+    ;
+    py::class_<SpotLight, SpotLightPtr, Light>(m, "SpotLight")
+    .def(py::init<>())
+    ;
+    py::class_<MarkerDevice, MarkerDevicePtr, Device>(m, "MarkerDevice")
+    .def(py::init<>())
+    ;
+    py::class_<ZmpDevice, ZmpDevicePtr, Device>(m, "ZmpDevice")
+    .def(py::init<>())
+    ;
+    py::class_<VisionSensor, VisionSensorPtr, Device>(m, "VisionSensor")
+    ;
+    py::class_<Camera, CameraPtr, VisionSensor>(m, "Camera")
+    .def(py::init<>())
+    ;
+    py::class_<RangeSensor, RangeSensorPtr, VisionSensor>(m, "RangeSensor")
+    .def(py::init<>())
     ;
 
     PyDeviceList<Device>(m, "DeviceList");
